8d0ff5f126a5cdde0f6008b44cb8cf0bf8a415e4
NCPEREIRA <NCPereira@users.noreply.github.com>
2019-11-08 22:16:50 +0000

    Upload exercicios

---
+++ 7_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void capitalizar(char str[]){
+  for(int i = 0; str[i] != '\0'; i++){
+    if(isalpha(str[i])){
+      str[i] = toupper(str[i]);
+    }
+  }
+  return;
+}
+
+int main(void){
+  char s[] = "To be or not to be, that is the question.";
+  capitalizar(s);
+  printf("%s\n", s);
+  return 0;
+}
