5092627b73b5b91899b3017a5a8c5022dbf4df66
NCPEREIRA <NCPereira@users.noreply.github.com>
2019-11-20 09:22:55 +0000

    Update ex. 7.1

--- 7_1.c
+++ 7_1.c
@@ -3,9 +3,7 @@
 
 void capitalizar(char str[]){
   for(int i = 0; str[i] != '\0'; i++){
-    if(isalpha(str[i])){
-      str[i] = toupper(str[i]);
-    }
+    str[i] = toupper(str[i]);
   }
   return;
 }
